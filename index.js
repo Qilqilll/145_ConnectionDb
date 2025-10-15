b3422752fb73b8246e65812b7451b505a3d68a1e
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-10-15 12:36:39 +0700

    Setup project

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
-count express = require('express');
-let mysql = require('mysql');
+const express = require('express');
+let mysql = require('mysql2');
 const PORT = process.env.PORT || 3000;
+const app = express();
 
-application.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    port: '3303',
-    password: 'Qilqil.123',
-    database: 'Mahasiswa'
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL database:', err.stack);
-        return;
-    }
-    console.log('Connected to MySQL database succesfully' + db.threadId);
-
-});
-
-const application = express();
-
-application.listen(PORT, () => {
-    console.log(`Server is running on port http://${PORT}`);
-}
+app.use(express.json()); // Agar bisa menerima JSON dari POST
