eedb7d9b9911a99e67d862b0419e88f0194f05d5
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-10-15 12:37:18 +0700

    Konfigurasi koneksi MySQL

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json()); // Agar bisa menerima JSON dari POST
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: '3309',
+    password: 'Qilqil.123',
+    database: 'mahasiswa'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL database:', err.stack);
+        return;
+    }
+    console.log('Connected to MySQL database succesfully ' + db.threadId);
+});
